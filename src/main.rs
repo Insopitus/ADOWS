0ba6d77e0ce2444f73e20450d0047033198b1b98
Insopitus <jessefor@hotmail.com>
2022-01-19 23:00:09 +0800

    change port if the current one is in use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io};
 // use std::io::Write;
 use std::path::Path;
 
@@ -10,24 +10,33 @@ fn main() {
     let current_dir = env::current_dir().unwrap().to_string_lossy().to_string();
     let path = env::args().skip(1).next().unwrap_or(current_dir);
     let path = Path::new(&path);
-    // let mut info = String::new();
-    // if path.is_dir() {
-    //     visit_dir(path, &mut info).unwrap();
-    // } else if path.is_file() {
-    //     // info.push_str("is file")
-    //     info.push_str(path.to_str().unwrap());
-    // }
-    // // // if write to a file
-    // // let mut log_file = File::create("log.txt").unwrap();
-    // // log_file.write_all(info.as_bytes()).unwrap_or_else(|_| {});
+    let mut port = 8080;
+    loop {
+      let start = start_server(port,path);
+      match start {
+        Ok(_)=>{
+          break;
+        },
+        Err(e)=>{
+          match e.kind() {
+              io::ErrorKind::AddrInUse=>{
+                port+=1;
+                continue;
+              },
+              _=>{
+                println!("Server failed to start.");
+                break
+              }
+          }
+        }
+      }
+    }
 
-    // // else print in the console
-    // println!("{}",info);
-    let fr = FolderReader::new(path);
-    let mut server = FileServer::new(fr);
-    server
-        .listen(8080)
-        .expect("Error occured. Server is closing.");
     println!("Press Enter to continue.");
     std::io::stdin().read_line(&mut String::new()).unwrap();
+}
+fn start_server(port: i32,path:&Path)->Result<(),io::Error> {
+    let fr = FolderReader::new(path);
+    let mut server = FileServer::new(fr);
+    server.listen(port)
 }
