85ab09ee77c7be5f8212e67052ce5abbe3d0b527
Insopitus <jessefor@hotmail.com>
2022-01-21 20:27:50 +0800

    reorganize code

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,29 @@
-use std::{env, io};
 use std::path::Path;
+use std::{env, io};
 
-use adows::mods::file_server::FileServer;
-use adows::mods::folder_reader::FolderReader;
-
+use adows::FileServer;
 
 fn main() {
-
     let current_dir = env::current_dir().unwrap().to_string_lossy().to_string();
     let path = env::args().skip(1).next().unwrap_or(current_dir);
     let path = Path::new(&path);
     let mut port = 8080;
+    let mut server = FileServer::new(path);
     loop {
-      let start = start_server(port,path);
-      match start {
-        Ok(_)=>{
-          break;
-        },
-        Err(e)=>{
-          match e.kind() {
-              io::ErrorKind::AddrInUse=>{
-                port+=1;
-                continue;
-              },
-              _=>{
-                println!("Server failed to start.");
-                break
-              }
+        let start = server.listen(port);
+        if let Err(e)=start {
+          if e.kind() == io::ErrorKind::AddrInUse {
+              port += 1;
+              continue;
+          } else {
+              println!("Server failed to start.");
+              break;
           }
+        } else {
+            break;
         }
-      }
     }
     println!("Press Enter to continue.");
     std::io::stdin().read_line(&mut String::new()).unwrap();
 }
-fn start_server(port: i32,path:&Path)->Result<(),io::Error> {
-    let fr = FolderReader::new(path);
-    let mut server = FileServer::new(fr);
-    server.listen(port)
-}
+
--- src/mods/mod.rs
+++ src/mods/mod.rs
@@ -1,5 +1,4 @@
 pub mod folder_reader;
-pub mod file_server;
 pub mod request_header;
 pub mod media_type;
 pub mod utils;
